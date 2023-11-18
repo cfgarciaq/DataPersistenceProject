2a938ae74a65fc90faf48f57e5735ce3cc7da0cf
cfgarciaq <cfgarciaq@gmail.com>
2023-11-19 00:17:05 +0100

    Update MenuHandler
    Added StartGame function which is called by the StartGame button OnClick event.  The function loads the 'main' game scene if the content of the player's name InputField is valid; otherwise it displays a warning message telling the user to enter name to play.
    
    Added DisplayUserErrorMessage and HideErrorMessage functions.

--- Assets/Scripts/UI/MenuHandler.cs
+++ Assets/Scripts/UI/MenuHandler.cs
@@ -1,26 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuHandler : MonoBehaviour
 {
+    [SerializeField] private Text userErrorMessage;
+    [SerializeField] private InputField playerNameInputField;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-
-        
-    }
+        HideUserErrorMessage();        
+    }    
 
     public void StartGame()
     {
         //to do
+        bool canStartGame = DataManager.Instance.SetNewPlayerName(playerNameInputField.text);
+        if (canStartGame)
+        {
+            SceneManager.LoadScene("main");
+        }
+        else
+        {
+            //Display error message for User for a couple seconds
+            float delay = 5;
+            string msg = $"You Must Enter a Player Name to Play";
+            DisplayUserErrorMessage(msg, delay);
+        }
     }
 
+    private void DisplayUserErrorMessage(string msg, float delay)
+    {
+        userErrorMessage.gameObject.SetActive(true);
+        userErrorMessage.text = msg;
+        //call hide method after 5secs
+        Invoke(nameof(HideUserErrorMessage), delay);
+    }
+
+    private void HideUserErrorMessage()
+    {
+        userErrorMessage.text = "";
+        userErrorMessage.gameObject.SetActive(false);
+    }
 }
